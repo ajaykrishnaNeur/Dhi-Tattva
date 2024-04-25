3e727224a0eccb9bf74ddace929267417913316f
Julika <132730416+Julika07@users.noreply.github.com>
2024-04-25 13:09:42 +0530

    dhi tattva

--- Assets/Scripts/APIManager.cs
+++ Assets/Scripts/APIManager.cs
@@ -7,45 +7,68 @@ public class APIManager : MonoBehaviour
 {
     private DataHandler dataHandler;
     public string activeApi;
-    public string loginApi;
     private string Response;
 
     
     void Start()
     {
         dataHandler = GameObject.Find("Data Handler").GetComponent<DataHandler>();
-        StartCoroutine(GetRequest(activeApi));
+       
     }
 
-    IEnumerator GetRequest(string url)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
-        {
-            // Request and wait for the desired page.
-            yield return webRequest.SendWebRequest();
+    //IEnumerator GetRequest(string url)
+    //{
+    //    using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+    //    {
+    //        // Request and wait for the desired page.
+    //        yield return webRequest.SendWebRequest();
 
-            if (webRequest.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError("GET request failed: " + webRequest.error);
-                dataHandler.LoginPanelActive();
-            }
-            else
-            {
-                // Print the received data
-                Debug.Log("GET request successful: " + webRequest.downloadHandler.text);
-            }
-        }
-    }
+    //        if (webRequest.result != UnityWebRequest.Result.Success)
+    //        {
+    //            Debug.LogError("GET request failed: " + webRequest.error);
+    //            dataHandler.LoginPanelActive();
+    //        }
+    //        else
+    //        {
+    //            // Print the received data
+    //            Debug.Log("GET request successful: " + webRequest.downloadHandler.text);
+    //        }
+    //    }
+    //}
 
 
-    public IEnumerator PostRequest(string url, string jsonData)
+    public IEnumerator DeviceIdPostRequest(string url, string jsonData)
     {
         using (UnityWebRequest webRequest = new UnityWebRequest(url, "POST"))
         {
             byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
             webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
             webRequest.downloadHandler = new DownloadHandlerBuffer();
-            webRequest.SetRequestHeader("Content-Type", "application/json");
+            webRequest.SetRequestHeader("device-id", SystemInfo.deviceUniqueIdentifier);
+
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("POST request failed: " + webRequest.error);
+                dataHandler.LoginPanelActive();
+            }
+            else
+            {
+                Response = webRequest.downloadHandler.text;
+            }
+
+        }
+    }
+
+    public IEnumerator LoginPostRequest(string url, string jsonData)
+    {
+        using (UnityWebRequest webRequest = new UnityWebRequest(url, "POST"))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+            webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
 
             // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
@@ -57,7 +80,6 @@ public class APIManager : MonoBehaviour
             else
             {
                 Response = webRequest.downloadHandler.text;
-                dataHandler.JsonParserData(Response);
             }
 
         }
--- Assets/Scripts/DataHandler.cs
+++ Assets/Scripts/DataHandler.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+using static DataHandler;
 
 public class DataHandler : MonoBehaviour
 {
@@ -8,12 +12,33 @@ public class DataHandler : MonoBehaviour
     public GameObject verificationPanel;
     public GameObject AdminPanel;
 
+    public TMP_InputField code_Inputfield;
     private APIManager apiManager;
+
+    public class DeviceIdFetch
+    {
+        public string DeviceId;
+    }
+
+    public class LoginCode
+    {
+        public string code;
+        public string DeviceId;
+    }
     // Start is called before the first frame update
     void Start()
     {
         apiManager = GameObject.Find("Api Manager").GetComponent<APIManager>();
+        DeviceId();
+    }
 
+    public void DeviceId()
+    {
+        DeviceIdFetch deviceIdFetch = new DeviceIdFetch();
+        deviceIdFetch.DeviceId = SystemInfo.deviceUniqueIdentifier;
+
+        string jsonData = JsonConvert.SerializeObject(deviceIdFetch);
+        StartCoroutine(apiManager.DeviceIdPostRequest("http://43.204.38.188:8000/v1/packages/active_package", jsonData));
     }
 
     public void JsonParserData(string _webData)
@@ -27,5 +52,15 @@ public class DataHandler : MonoBehaviour
         AdminPanel.SetActive(true);
         Numpad.SetActive(true);
         submitBtn.SetActive(true);
+    }
+
+    public void Login()
+    {
+        LoginCode loginCode = new LoginCode();
+        loginCode.DeviceId = SystemInfo.deviceUniqueIdentifier;
+        loginCode.code = code_Inputfield.text;
+
+        string jsonData = JsonConvert.SerializeObject(loginCode);
+        StartCoroutine(apiManager.LoginPostRequest("http://43.204.38.188:8000/v1/packages/active_package", jsonData));
     }
 }
